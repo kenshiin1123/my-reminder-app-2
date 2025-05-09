3cde2466537494f79025a8eef744bcdcab703684
Lance Ivan Gil Fernandez <fernandezlanceivangil@gmail.com>
2025-05-09 19:08:20 +0800

    modify and fix minor backend bugs

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.config.js";
+import { verifyAccessToken } from "./controller/auth.controller.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ import authRoutes from "./routes/auth.routes.js"; // Authentication Route
 import userRoutes from "./routes/user.routes.js"; // User Route
 import reminderRoutes from "./routes/reminder.routes.js"; // Reminder Route
 
+// Middlewares
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -25,8 +27,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/reminders", reminderRoutes);
+app.use("/api/users", verifyAccessToken, userRoutes);
+app.use("/api/reminders", verifyAccessToken, reminderRoutes);
 
 app.listen(PORT, () => {
   console.log("Listening to port", PORT);
